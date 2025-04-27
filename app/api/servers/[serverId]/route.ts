cfcb5a5dc584cbbb6fb71a55082e364a73328f88
pb.liu <liuhoom@gmail.com>
2025-04-27 09:05:24 +0000

    chore: add edit server route

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,36 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile()
+
+    if (!profile) return new NextResponse('Unauthorized.', { status: 401 })
+
+    if (!params.serverId)
+      return new NextResponse('Server id is required.', { status: 400 })
+
+    const { name, imageUrl } = await req.json()
+
+    const server = db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    })
+
+    return NextResponse.json(server)
+  } catch (error) {
+    console.log(error)
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
